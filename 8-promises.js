96a893c0f64a0bb1ccdf03f7f9d3ec6a4f4f22ac
capkloss72 <tym.zon@gmail.com>
2021-05-29 11:46:07 +0800

    Added promise chaining examples

--- 8-promises.js
+++ 8-promises.js
@@ -1,14 +1,46 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    //resolve([1, 4, 7]);
-    reject('Things went badly with the promise');
-  }, 2000);
-});
+// const doWorkPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     //resolve([1, 4, 7]);
+//     reject('Things went badly with the promise');
+//   }, 2000);
+// });
 
-doWorkPromise
-  .then((result) => {
-    console.log(`Successfully called promise: ${result}`, result);
+// doWorkPromise
+//   .then((result) => {
+//     console.log(`Successfully called promise: ${result}`, result);
+//   })
+//   .catch((error) => {
+//     console.log(`${error}`);
+//   });
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
+// add(2, 2)
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+add(2, 2)
+  .then((sum) => {
+    console.log(`first sum ${sum}`);
+    return add(sum, 2);
   })
-  .catch((error) => {
-    console.log(`${error}`);
+  .then((sum2) => {
+    console.log(`second sum ${sum2}`);
+    return add(sum2, 2);
+  })
+  .then((sum3) => {
+    console.log(`third sum ${sum3}`);
+  })
+  .catch((e) => {
+    console.log(e);
   });
